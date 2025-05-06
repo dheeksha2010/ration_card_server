bec3579d1ff4e3c5363c3e18c3ace8d70f60575f
dheeksha2010 <tseva.enr@gmail.com>
2025-05-06 19:34:31 +0530

    first commit

--- server_2.js
+++ server_2.js
@@ -23,7 +23,7 @@ app.post('/api/getRationData', async (req, res) => {
         console.log(`[${timestamp}] ${message}`);
     };
 
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
     try {
